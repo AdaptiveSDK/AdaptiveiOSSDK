6c8f71159fc47ba85dac808e8c8f21c98c3c31d8
mohamed.haloka <mohamed.haloka@nahdetmisr.com>
2026-04-05 11:03:06 +0200

    refactor(analytics): remove ipAddress, userAgent, clientId, eventTimestamp from RegistrationEvent and LoginEvent

--- Sources/AdaptiveSDK/AdaptiveAnalytics/Sources/AdaptiveAnalytics/Events/LoginEvent.swift
+++ Sources/AdaptiveSDK/AdaptiveAnalytics/Sources/AdaptiveAnalytics/Events/LoginEvent.swift
@@ -1,30 +1,18 @@
 public struct LoginEvent: Encodable {
-    public let userId        : Int
-    public let userEmail     : String
-    public let userFullName  : String
-    public let clientId      : String
-    public let eventTimestamp: Int
-    public let ipAddress     : String
-    public let userAgent     : String
-    public let productId     : Int
+    public let userId       : Int
+    public let userEmail    : String
+    public let userFullName : String
+    public let productId    : Int
 
     public init(
-        userId        : Int,
-        userEmail     : String,
-        userFullName  : String,
-        clientId      : String,
-        eventTimestamp: Int,
-        ipAddress     : String,
-        userAgent     : String,
-        productId     : Int
+        userId      : Int,
+        userEmail   : String,
+        userFullName: String,
+        productId   : Int
     ) {
-        self.userId         = userId
-        self.userEmail      = userEmail
-        self.userFullName   = userFullName
-        self.clientId       = clientId
-        self.eventTimestamp = eventTimestamp
-        self.ipAddress      = ipAddress
-        self.userAgent      = userAgent
-        self.productId      = productId
+        self.userId       = userId
+        self.userEmail    = userEmail
+        self.userFullName = userFullName
+        self.productId    = productId
     }
 }
--- Sources/AdaptiveSDK/AdaptiveAnalytics/Sources/AdaptiveAnalytics/Events/RegistrationEvent.swift
+++ Sources/AdaptiveSDK/AdaptiveAnalytics/Sources/AdaptiveAnalytics/Events/RegistrationEvent.swift
@@ -1,33 +1,21 @@
 public struct RegistrationEvent: Encodable {
-    public let userId        : Int
-    public let userEmail     : String
-    public let userFullName  : String
-    public let clientId      : String
-    public let eventTimestamp: Int
-    public let productId     : Int
-    public let ipAddress     : String
-    public let userAgent     : String
-    public let phoneNumber   : String
+    public let userId       : Int
+    public let userEmail    : String
+    public let userFullName : String
+    public let productId    : Int
+    public let phoneNumber  : String
 
     public init(
-        userId        : Int,
-        userEmail     : String,
-        userFullName  : String,
-        clientId      : String,
-        eventTimestamp: Int,
-        productId     : Int,
-        ipAddress     : String,
-        userAgent     : String,
-        phoneNumber   : String
+        userId      : Int,
+        userEmail   : String,
+        userFullName: String,
+        productId   : Int,
+        phoneNumber : String
     ) {
-        self.userId         = userId
-        self.userEmail      = userEmail
-        self.userFullName   = userFullName
-        self.clientId       = clientId
-        self.eventTimestamp = eventTimestamp
-        self.productId      = productId
-        self.ipAddress      = ipAddress
-        self.userAgent      = userAgent
-        self.phoneNumber    = phoneNumber
+        self.userId       = userId
+        self.userEmail    = userEmail
+        self.userFullName = userFullName
+        self.productId    = productId
+        self.phoneNumber  = phoneNumber
     }
 }
